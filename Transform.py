883e737ba3738e3c51ab5acdd6e983273d0d53f7
FelipeMP94 <felipe00depaula@gmail.com>
2024-01-12 20:55:49 -0300

    optimization

--- Transform.py
+++ Transform.py
@@ -1,56 +1,45 @@
 import pandas as pd
+import time
 
 dado_total = pd.read_csv(r"C:\Users\Felipe\Documents\Dados TSE\perfil_eleitor_secao_ATUAL_SP\perfil_eleitor_secao_ATUAL_SP.csv",encoding = 'latin1',sep = ';')
 
 def Quantidade_por_grau_escolaridade(zona):
     qt_escolaridade = {}
-    qt = 0 
     for grau in zona['DS_GRAU_ESCOLARIDADE'].unique():
-        for row in zona[zona['DS_GRAU_ESCOLARIDADE']== grau ].itertuples():
-            qt += row.QT_ELEITORES_PERFIL
-        qt_escolaridade[grau] = qt
-        qt = 0
+        dataloc = zona.loc[zona['DS_GRAU_ESCOLARIDADE']== grau]
+        qt_escolaridade[grau] = dataloc['QT_ELEITORES_PERFIL'].sum()
     return qt_escolaridade
 
 
 def Quantidade_por_faixa_etaria(zona):
     qt_faixa_etaria = {}
-    qt = 0  
     for faixa in zona['DS_FAIXA_ETARIA'].unique():
-        for row in zona[zona['DS_FAIXA_ETARIA']== faixa ].itertuples():
-            qt += row.QT_ELEITORES_PERFIL
-        qt_faixa_etaria[faixa] = qt
-        qt = 0
+        dataloc =  zona.loc[zona['DS_FAIXA_ETARIA']== faixa ]
+        qt_faixa_etaria[faixa] = dataloc['QT_ELEITORES_PERFIL'].sum()
     return qt_faixa_etaria
 
 
 
 def Quantidade_por_estado_civil(zona):
     qt_estado_civil = {}
-    qt = 0 
     for estado in zona['DS_ESTADO_CIVIL'].unique():
-        for row in zona[zona['DS_ESTADO_CIVIL']== estado ].itertuples():
-            qt += row.QT_ELEITORES_PERFIL
-        qt_estado_civil[estado] = qt
-        qt = 0
+        dataloc =  zona.loc[zona['DS_ESTADO_CIVIL']== estado ]
+        qt_estado_civil[estado] = dataloc['QT_ELEITORES_PERFIL'].sum()
     return qt_estado_civil
 
 
 
 def Quantidade_por_genero(zona):
     qt_genero = {}
-    qt = 0 
     for t_gen in zona['DS_GENERO'].unique():
-        for row in zona[zona['DS_GENERO']== t_gen ].itertuples():
-            qt += row.QT_ELEITORES_PERFIL
-        qt_genero[t_gen] = qt
-        qt = 0
+        dataloc =  zona.loc[zona['DS_GENERO']== t_gen ]
+        qt_genero[t_gen] = dataloc['QT_ELEITORES_PERFIL'].sum()
     return qt_genero
 
 #Principal 
 
 dados_zonas_eleitorais = []
-
+ini = time.time()
 for z in dado_total['NR_ZONA'].unique():
     zona = dado_total[dado_total['NR_ZONA']== z]
     info_zonas = {'id_zona':z ,
@@ -61,7 +50,7 @@ for z in dado_total['NR_ZONA'].unique():
                  }
     
     dados_zonas_eleitorais.append(info_zonas)
-
-print(dados_zonas_eleitorais)
+fim = time.time()
+print(fim-ini)
 
 
